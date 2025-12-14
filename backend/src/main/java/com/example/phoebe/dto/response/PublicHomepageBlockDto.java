803902af1966635d2fac29691046e2d11897a91b
Roman Konoplev <rkonoplev@gmail.com>
2025-12-14 10:40:47 +0000

    Fix PublicHomepageBlockDto.java

--- backend/src/main/java/com/example/phoebe/dto/response/PublicHomepageBlockDto.java
+++ backend/src/main/java/com/example/phoebe/dto/response/PublicHomepageBlockDto.java
@@ -1,8 +1,6 @@
 package com.example.phoebe.dto.response;
 
 import com.example.phoebe.model.HomePageBlockType;
-import com.example.phoebe.dto.response.PublicNewsDto;
-
 import java.util.List;
 
 public class PublicHomepageBlockDto {
@@ -18,24 +16,72 @@ public class PublicHomepageBlockDto {
     private Boolean showTeaser;
     private String titleFontSize;
 
-    // Constructors
-    public PublicHomepageBlockDto() {}
+    public PublicHomepageBlockDto() {
+    }
 
-    // Getters
-    public Integer getId() { return id; }
-    public Integer getWeight() { return weight; }
-    public HomePageBlockType getBlockType() { return blockType; }
-    public String getContent() { return content; }
-    public List<PublicNewsDto> getNews() { return news; }
-    public Boolean getShowTeaser() { return showTeaser; }
-    public String getTitleFontSize() { return titleFontSize; }
+    public PublicHomepageBlockDto(Integer id, Integer weight, HomePageBlockType blockType, String content, List<PublicNewsDto> news, Boolean showTeaser, String titleFontSize) {
+        this.id = id;
+        this.weight = weight;
+        this.blockType = blockType;
+        this.content = content;
+        this.news = news;
+        this.showTeaser = showTeaser;
+        this.titleFontSize = titleFontSize;
+    }
 
-    // Setters
-    public void setId(Integer id) { this.id = id; }
-    public void setWeight(Integer weight) { this.weight = weight; }
-    public void setBlockType(HomePageBlockType blockType) { this.blockType = blockType; }
-    public void setContent(String content) { this.content = content; }
-    public void setNews(List<PublicNewsDto> news) { this.news = news; }
-    public void setShowTeaser(Boolean showTeaser) { this.showTeaser = showTeaser; }
-    public void setTitleFontSize(String titleFontSize) { this.titleFontSize = titleFontSize; }
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public Integer getWeight() {
+        return weight;
+    }
+
+    public void setWeight(Integer weight) {
+        this.weight = weight;
+    }
+
+    public HomePageBlockType getBlockType() {
+        return blockType;
+    }
+
+    public void setBlockType(HomePageBlockType blockType) {
+        this.blockType = blockType;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public List<PublicNewsDto> getNews() {
+        return news;
+    }
+
+    public void setNews(List<PublicNewsDto> news) {
+        this.news = news;
+    }
+
+    public Boolean getShowTeaser() {
+        return showTeaser;
+    }
+
+    public void setShowTeaser(Boolean showTeaser) {
+        this.showTeaser = showTeaser;
+    }
+
+    public String getTitleFontSize() {
+        return titleFontSize;
+    }
+
+    public void setTitleFontSize(String titleFontSize) {
+        this.titleFontSize = titleFontSize;
+    }
 }
